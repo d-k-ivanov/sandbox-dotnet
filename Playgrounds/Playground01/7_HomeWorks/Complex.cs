605f3bff0ece566edc63193c691660594de8afcc
Dmitriy Ivanov <d.k.ivanov@live.com>
2020-06-28 13:28:18 +0300

    implement simple class for Complex numbers

---
+++ Playgrounds/Playground01/7_HomeWorks/Complex.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_HomeWorks
+{
+    public class Complex
+    {
+        public double Real { get; }
+        public double Imaginary { get; }
+
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        private Complex()
+        {
+        }
+
+        public Complex Plus(Complex c)
+        {
+            return new Complex(this.Real + c.Real, this.Imaginary + c.Imaginary);
+        }
+
+        public static Complex operator+(Complex c1, Complex c2)
+        {
+            if (c1 == null) throw new ArgumentNullException(nameof(c1));
+            if (c2 == null) throw new ArgumentNullException(nameof(c2));
+            return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        }
+
+        public Complex Minus(Complex c)
+        {
+            return new Complex(this.Real - c.Real, this.Imaginary - c.Imaginary);
+        }
+
+        public static Complex operator-(Complex c1, Complex c2)
+        {
+            if (c1 == null) throw new ArgumentNullException(nameof(c1));
+            if (c2 == null) throw new ArgumentNullException(nameof(c2));
+            return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        }
+
+        public override string ToString()
+        {
+            if (Imaginary < 0)
+            {
+                return $"({Real} - {Math.Abs(Imaginary)}i)";
+            }
+
+            if (Math.Abs(Imaginary) < 0.0000001 && Math.Abs(Imaginary) > -0.0000001)
+            {
+                return $"{Real}";
+            }
+
+            return $"({Real} + {Math.Abs(Imaginary)}i)";
+        }
+    }
+
+
+}
---
+++ Playgrounds/Playground01/7_HomeWorks/Program.cs
@@ -0,0 +1,109 @@
+using System;
+using CSCLib;
+
+namespace _7_HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleUtils.PrintSeparator();
+
+            StartAll();
+            // ComplexNumbers();
+
+            // -----------------------------------------------
+            ConsoleUtils.EndOfProgram();
+        }
+
+        private static void StartAll()
+        {
+            ComplexNumbers();
+            // ConsoleUtils.PrintSeparator();
+        }
+
+        private static void ComplexNumbers()
+        {
+            var c1 = new Complex(1,1);
+            var c2 = new Complex(1,2);
+
+            var result = c1.Plus(c2);
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            c1 = new Complex(5.245,4.52);
+            c2 = new Complex(11.57,-12.11);
+
+            result = c1.Plus(c2);
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            c1 = new Complex(11,0);
+            c2 = new Complex(-6,0);
+
+            result = c1.Plus(c2);
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+            c1 = new Complex(1,1);
+            c2 = new Complex(1,2);
+
+            result = c1.Minus(c2);
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+
+            c1 = new Complex(5.245,4.52);
+            c2 = new Complex(11.57,-12.11);
+
+            result = c1.Minus(c2);
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+
+            c1 = new Complex(11,0);
+            c2 = new Complex(-6,0);
+
+            result = c1.Minus(c2);
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+            c1 = new Complex(1,1);
+            c2 = new Complex(1,2);
+
+            result = c1 + c2;
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            c1 = new Complex(5.245,4.52);
+            c2 = new Complex(11.57,-12.11);
+
+            result = c1 + c2;
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            c1 = new Complex(11,0);
+            c2 = new Complex(-6,0);
+
+            result = c1 + c2;
+            Console.WriteLine($"{c1} + {c2} = {result}\n");
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+            c1 = new Complex(1,1);
+            c2 = new Complex(1,2);
+
+            result = c1 - c2;
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+
+            c1 = new Complex(5.245,4.52);
+            c2 = new Complex(11.57,-12.11);
+
+            result = c1 - c2;
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+
+            c1 = new Complex(11,0);
+            c2 = new Complex(-6,0);
+
+            result = c1 - c2;
+            Console.WriteLine($"{c1} - {c2} = {result}\n");
+        }
+    }
+}
